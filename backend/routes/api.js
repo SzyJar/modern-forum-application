b9e83dbce0532e7e206ab1fa5adff1b00766bec4
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-09-06 10:06:58 +0200

    update message by ID

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -45,6 +45,7 @@ module.exports = function (app) {
                     });
                     await newUser.save();
                     req.session.userId = newUser._id;
+                    req.session.userName = newUser.name;
                     return res.status(201).json({ name: newUser.name, avatar: newUser.avatar });
                 };
             } catch (err) {
@@ -57,6 +58,7 @@ module.exports = function (app) {
                     const isValidPassword = await bcrypt.compareSync(password, user.password);
                     if(isValidPassword) {
                         req.session.userId = user._id;
+                        req.session.userName = user.name;
                         return res.status(200).json({ name: user.name, avatar: user.avatar });
                     };
                     return res.status(401).end('Incorrect password');
@@ -111,6 +113,50 @@ module.exports = function (app) {
                 // Client is trying to access other's private chat
                 return res.status(404).end('Chat does not exist');
             };
+        } catch(err) {
+            console.error('Error occurred in POST request to /message/:chatname', err.message);
+            res.status(500).end('An error occurred while creating new message');
+        };
+    });
+
+    app.route('/message/:chatname')
+    // Edit a message
+    .put(auth, async function(req, res) {
+        const name = req.params.chatname;
+        const message_id = req.body.message_id;
+        const content = req.body.content;
+        try {
+            const chat = await Chat.findOne({ name: name });
+
+            if(!chat) {
+                return res.status(404).end('Chat does not exist');
+            };
+
+            if (!chat.users.length === 0 && !chat.users.includes(req.session.userId)) {
+                // Client is trying to access other's private chat
+                return res.status(404).end('Chat does not exist');
+            };
+
+            const messageSender = chat.messages.find((message) => {
+                return message.sender === req.session.userName && message._id.toString() === message_id;
+            });
+
+            if(!messageSender) {
+                return res.status(404).end('Chat message does not exist');
+            };
+
+            const message = await Chat.findOneAndUpdate(
+                { name: name, 'messages._id': message_id },
+                {
+                  $set: {
+                    'messages.$.content': content + ' [EDITED]',
+                    'messages.$.timestamp': new Date(),
+                  },
+                },
+                { new: true },
+            );
+
+            return res.json(message);
         } catch(err) {
             console.error('Error occurred in POST request to /message/:chatname', err.message);
             res.status(500).end('An error occurred while creating new message');
